f1e2f0b67c56e86969056be5cbe14bad05bd6bec
Averageasd <nhuynh9497@gmail.com>
2026-03-31 15:33:15 -0400

    finished chap 3

---
+++ chap3/dags/interval_crone_dag.py
@@ -0,0 +1,56 @@
+from pathlib import Path
+
+import pandas as pd
+import pendulum
+from airflow.providers.standard.operators.bash import BashOperator
+from airflow.providers.standard.operators.python import PythonOperator
+from airflow.sdk import DAG
+from airflow.timetables.interval import CronDataIntervalTimetable
+
+
+def _calculate_stats(input_path, output_path):
+    """Calculates event statistics."""
+
+    events = pd.read_json(input_path, convert_dates=["timestamp"])
+
+    stats = (
+        events.assign(date=lambda df: df["timestamp"].dt.date).groupby(["date", "user"]).size().reset_index()
+    )
+
+    Path(output_path).parent.mkdir(exist_ok=True)
+    stats.to_csv(output_path, index=False)
+
+
+with DAG(
+    
+    # process data for the specified interval [start_date, end_date)
+    # we can process data for missing dates with catchup=True
+    dag_id="05_interval_cron",
+    schedule=CronDataIntervalTimetable("0 0 * * *", timezone="UTC"),
+    start_date=pendulum.datetime(year=2026, month=1, day=1),
+    end_date=pendulum.datetime(year=2026, month=1, day=5),
+    catchup=True,
+):
+    
+    # use logical date since we want to only calculate statistic
+    # for current json file we just fetch
+    fetch_events = BashOperator(
+        task_id="fetch_events",
+        bash_command=(
+            "mkdir -p /data/05_interval_cron/events/ && "
+            "curl -o /data/05_interval_cron/events/{{ logical_date | ds }}.json "
+            "'http://events-api:8081/events/range?"
+            "start_date={{ data_interval_start | ds }}&end_date={{ data_interval_end | ds }}'"
+        ),
+    )
+
+    calculate_stats = PythonOperator(
+        task_id="calculate_stats",
+        python_callable=_calculate_stats,
+        op_kwargs={
+            "input_path": "/data/05_interval_cron/events/{{ logical_date | ds }}.json",
+            "output_path": "/data/05_interval_cron/stats/{{ logical_date | ds }}.csv",
+        },
+    )
+
+    fetch_events >> calculate_stats
