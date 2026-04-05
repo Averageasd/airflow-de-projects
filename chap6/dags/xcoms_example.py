025fd2952a4f0007a4d3c5afa48aea56a61f4571
Averageasd <nhuynh9497@gmail.com>
2026-04-05 16:13:32 -0400

    finished chap 6

---
+++ chap6/dags/xcoms_example.py
@@ -0,0 +1,51 @@
+import uuid
+
+import pendulum
+from airflow.providers.standard.operators.empty import EmptyOperator
+from airflow.providers.standard.operators.python import PythonOperator
+from airflow.sdk import DAG
+
+def _train_model(**context):
+    model_id = str(uuid.uuid4())
+    
+    # store the value with key and id in task_instance
+    context['task_instance'].xcom_push(key="model_id", value=model_id)
+
+def _deploy_model(templates_dict, **context):
+    
+    # fetch value from xcom
+    # model_id = context['task_instance'].xcom_pull(task_ids="train_model", key="model_id")
+    
+    # using templates_dict to reference xcom 
+    model_id = templates_dict['model_id']
+    print(f"Deploying model {model_id}")
+
+with DAG(
+    dag_id="09_xcoms",
+    start_date=pendulum.today("UTC").add(days=-3),
+    schedule="@daily",
+    catchup=True,
+):
+    start = EmptyOperator(task_id="start")
+
+    fetch_sales = EmptyOperator(task_id="fetch_sales")
+    clean_sales = EmptyOperator(task_id="clean_sales")
+
+    fetch_weather = EmptyOperator(task_id="fetch_weather")
+    clean_weather = EmptyOperator(task_id="clean_weather")
+
+    join_datasets = EmptyOperator(task_id="join_datasets")
+
+    train_model = PythonOperator(task_id="train_model", python_callable=_train_model)
+
+    deploy_model = PythonOperator(
+        task_id="deploy_model", 
+        python_callable=_deploy_model,
+        templates_dict = {"model_id": "{{task_instance.xcom_pull(task_ids='train_model', key='model_id')}}"},
+    )
+
+    start >> [fetch_sales, fetch_weather]
+    fetch_sales >> clean_sales
+    fetch_weather >> clean_weather
+    [clean_sales, clean_weather] >> join_datasets
+    join_datasets >> train_model >> deploy_model
