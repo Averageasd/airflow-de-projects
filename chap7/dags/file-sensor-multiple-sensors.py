7c159b3da72c9d55780b9e17b36e27e0b72eff32
Averageasd <nhuynh9497@gmail.com>
2026-04-06 15:07:07 -0400

    finished chap 7

---
+++ chap7/dags/file-sensor-multiple-sensors.py
@@ -0,0 +1,26 @@
+
+import pendulum
+from airflow.sdk import DAG
+from airflow.providers.standard.operators.empty import EmptyOperator
+from airflow.providers.standard.sensors.filesystem import FileSensor
+from airflow.timetables.trigger import CronTriggerTimetable
+
+with DAG(
+    dag_id="03_file_sensor_example",
+    start_date=pendulum.today("UTC").add(days=-3),
+    schedule=CronTriggerTimetable("0 16 * * *", timezone="UTC"),
+    description="A batch workflow for ingesting supermarket promotions data, demonstrating the FileSensor.",
+    default_args={"depends_on_past": True},
+):
+    create_metrics = EmptyOperator(task_id="create_metrics")
+
+    # create multiple file sensors for each supermarket.
+    # each file sensor will poll for file specified with filepath. 
+    for supermarket_id in [1, 2, 3, 4]:
+        wait = FileSensor(
+            task_id=f"wait_for_supermarket_{supermarket_id}",
+            filepath=f"/data/supermarket{supermarket_id}/data.csv",
+        )
+        copy = EmptyOperator(task_id=f"copy_to_raw_supermarket_{supermarket_id}")
+        process = EmptyOperator(task_id=f"process_supermarket_{supermarket_id}")
+        wait >> copy >> process >> create_metrics
---
+++ chap7/dags/python-sensors.py
@@ -0,0 +1,30 @@
+
+from datetime import timedelta
+from pathlib import Path
+
+import pendulum
+from airflow.sdk import DAG
+from airflow.providers.standard.sensors.python import PythonSensor
+from airflow.timetables.trigger import CronTriggerTimetable
+
+
+def _wait_for_supermarket(supermarket_id_):
+    supermarket_path = Path("/data/" + supermarket_id_)
+    data_files = supermarket_path.glob("data-*.csv")
+    success_file = supermarket_path / "_SUCCESS"
+    return data_files and success_file.exists()
+
+
+with DAG(
+    dag_id="04_PythonSensor_example",
+    start_date=pendulum.today("UTC").add(days=-3),
+    schedule=CronTriggerTimetable("0 16 * * *", timezone="UTC"),
+    description="A batch workflow for ingesting supermarket promotions data.",
+    default_args={"depends_on_past": True},
+):
+    wait_for_supermarket_1 = PythonSensor(
+        task_id="wait_for_supermarket_1",
+        python_callable=_wait_for_supermarket,
+        op_kwargs={"supermarket_id_": "supermarket1"},
+        timeout=timedelta(minutes=5),
+    )
